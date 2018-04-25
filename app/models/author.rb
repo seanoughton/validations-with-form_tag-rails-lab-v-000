12dbf7a150af24968eee3861d2a609d9b9f17b2d
Sean Oughton <seanoughton@gmail.com>
2018-04-25 23:41:55 +0000

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, uniqueness: true
+  validates :phone_number, length: { minimum: 10 }
+  validates :phone_number, length: { maximum: 10 }
 end
